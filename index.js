5e3b04d4d19a48d80e9b50bcbf776d7adcbc3447
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-18 09:31:15 -0400

    adds security

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // let userSearch = prompt('What would you  like to see? ');
 let userSearch = 'cars';
-fetch(`https://api.unsplash.com/search/photos?page=1&query=${userSearch}&client_id=1b4430d53513bd836aae9081bc5ca45860ea77e0e70ec5215c2979fa908fd5f0`)
+fetch(`https://api.unsplash.com/search/photos?page=1&query=${userSearch}&client_id=${USKey}`)
 .then(r => r.json())
 .then(getUrls)
 .then(drawImages);
